84060c3522a777259bbcff67ea281d864ab03a50
Daniel Gouveia <Nel_hue@hotmail.com>
2024-03-12 19:31:43 -0300

    404 page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Blog from "./pages/Blog";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Blog />} path="/blog" />
+
+        <Route path="/404" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,53 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import LogoSimples from "../assets/LogoSimples";
+import React from "react";
+
 const ErrorPage = () => {
-  return <div></div>;
+  const [time, setTime] = React.useState(10);
+  const navigate = useNavigate();
+
+  const countDown = () => {
+    const interval = setInterval(() => {
+      if (time <= 1) {
+        clearInterval(interval);
+        navigate("/");
+        return;
+      }
+
+      setTime(time - 1);
+    }, 1000);
+    return interval;
+  };
+
+  React.useEffect(() => {
+    const interval = countDown();
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="bg-radial h-screen grid justify-items-center items-center box-border ">
+      <div className="grid gap-10 py-20 max-w-[400px] justify-items-center ">
+        <NavLink to="/">
+          <LogoSimples dark large />
+        </NavLink>
+        <div>
+          <h1 className=" text-7xl sm:text-8xl">404</h1>
+          <h2 className="text-2xl">Página não encontrada</h2>
+
+          <NavLink
+            to="/"
+            className="text-sky-600 flex items-center gap-2 justify-center">
+            Voltar ao inicio
+          </NavLink>
+        </div>
+
+        <div className="max-w-[400px] flex flex-col gap-4 justify-self-center text-lg">
+          <p>Redirecionando para o início em {time}s...</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
